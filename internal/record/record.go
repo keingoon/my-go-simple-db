cc728ea0e5fbda48cf9c0bfb2b9a3a845d051dd2
keingoon <keingoon@gmail.com>
2026-08-27 00:07:42 +0900

    add: record page

--- internal/record/record.go
+++ internal/record/record.go
@@ -1,9 +1,12 @@
 package record
 
 import (
+	"context"
+	"fmt"
 	"slices"
 
 	"github.com/keingoon/simpledb/internal/file"
+	"github.com/keingoon/simpledb/internal/trx/tx"
 )
 
 type FieldType int32
@@ -77,7 +80,7 @@ func (schema *Schema) AddAll(sch *Schema) {
 }
 
 type Layout struct {
-	Schema   *Schema
+	schema   *Schema
 	offsets  map[string]int32
 	slotSize int32
 }
@@ -85,7 +88,7 @@ type Layout struct {
 func NewLayOut(schema *Schema) *Layout {
 	offsets := make(map[string]int32)
 	layout := &Layout{
-		Schema:  schema,
+		schema:  schema,
 		offsets: offsets,
 	}
 
@@ -102,6 +105,10 @@ func NewLayOutFromCatalog(schema *Schema, offsets map[string]int32, slotsize int
 	return &Layout{schema, offsets, slotsize}
 }
 
+func (layout *Layout) Schema() *Schema {
+	return layout.schema
+}
+
 func (layout *Layout) Offset(fldname string) int32 {
 	return layout.offsets[fldname]
 }
@@ -111,10 +118,116 @@ func (layout *Layout) SlotSize() int32 {
 }
 
 func (layout *Layout) lengthInBytes(fldname string) int32 {
-	fldType := layout.Schema.FldType(fldname)
+	fldType := layout.schema.FldType(fldname)
 	if fldType == Integer {
 		return int32Size
 	}
-	strLen := layout.Schema.Length(fldname)
+	strLen := layout.schema.Length(fldname)
 	return int32(file.VarBytesLen(int(strLen)))
+}
+
+const (
+	Empty int32 = iota
+	Used
+)
+
+type RecordPage struct {
+	tx     *tx.TransactionMgr
+	blk    *file.BlockId
+	layout *Layout
+}
+
+func NewRecordPage(ctx context.Context, tx *tx.TransactionMgr, blk *file.BlockId, layout *Layout) *RecordPage {
+	tx.Pin(ctx, blk)
+	return &RecordPage{tx, blk, layout}
+}
+
+func (rp *RecordPage) GetInt(ctx context.Context, slot int32, fldname string) (int32, error) {
+	fldpos := rp.offset(slot) + rp.layout.Offset(fldname)
+	return rp.tx.GetInt32(ctx, rp.blk, fldpos)
+}
+
+func (rp *RecordPage) GetString(ctx context.Context, slot int32, fldname string) (string, error) {
+	fldpos := rp.offset(slot) + rp.layout.Offset(fldname)
+	return rp.tx.GetStr(ctx, rp.blk, fldpos)
+}
+
+func (rp *RecordPage) SetInt(ctx context.Context, slot int32, fldname string, val int32) error {
+	fldpos := rp.offset(slot) + rp.layout.Offset(fldname)
+	return rp.tx.SetInt32(ctx, rp.blk, fldpos, val, true)
+}
+
+func (rp *RecordPage) SetStr(ctx context.Context, slot int32, fldname string, val string) error {
+	fldpos := rp.offset(slot) + rp.layout.Offset(fldname)
+	return rp.tx.SetStr(ctx, rp.blk, fldpos, val, true)
+}
+
+func (rp *RecordPage) Delete(ctx context.Context, slot int32) error {
+	return rp.setFlag(ctx, slot, Empty)
+}
+
+func (rp *RecordPage) Format(ctx context.Context) {
+	slot := int32(0)
+	for rp.isValidSlot(slot) {
+		rp.tx.SetInt32(ctx, rp.blk, rp.offset(slot), Empty, false)
+		sch := rp.layout.Schema()
+		for _, fldname := range sch.Fields() {
+			fldpos := rp.offset(slot) + rp.layout.Offset(fldname)
+			if sch.FldType(fldname) == Integer {
+				rp.tx.SetInt32(ctx, rp.blk, fldpos, 0, false)
+			} else if sch.FldType(fldname) == Varchar {
+				rp.tx.SetStr(ctx, rp.blk, fldpos, "", false)
+			}
+		}
+	}
+}
+
+func (rp *RecordPage) NextAfter(ctx context.Context, slot int32) (int32, error) {
+	return rp.searchAfter(ctx, slot, Used)
+}
+
+func (rp *RecordPage) InsertAfter(ctx context.Context, slot int32) (int32, error) {
+	newslot, err := rp.searchAfter(ctx, slot, Empty)
+	if err != nil {
+		return -1, fmt.Errorf("searchafter error: %w", err)
+	}
+
+	if err := rp.setFlag(ctx, newslot, Used); err != nil {
+		return -1, fmt.Errorf("setflag error: %w", err)
+	}
+
+	return newslot, nil
+}
+
+func (rp *RecordPage) Block() *file.BlockId {
+	return rp.blk
+}
+
+func (rp *RecordPage) setFlag(ctx context.Context, slot int32, flag int32) error {
+	slotpos := rp.offset(slot)
+	return rp.tx.SetInt32(ctx, rp.blk, slotpos, flag, true)
+}
+
+func (rp *RecordPage) searchAfter(ctx context.Context, slot int32, flag int32) (int32, error) {
+	slot++
+	for rp.isValidSlot(slot) {
+		slotpos := rp.offset(slot)
+		slotFlag, err := rp.tx.GetInt32(ctx, rp.blk, slotpos)
+		if err != nil {
+			return -1, fmt.Errorf("getint32 error: %w", err)
+		}
+		if slotFlag == flag {
+			return slot, nil
+		}
+		slot++
+	}
+	return -1, nil
+}
+
+func (rp *RecordPage) isValidSlot(slot int32) bool {
+	return rp.offset(slot+1) <= rp.tx.Blocksize()
+}
+
+func (rp *RecordPage) offset(slot int32) int32 {
+	return slot * rp.layout.slotSize
 }
